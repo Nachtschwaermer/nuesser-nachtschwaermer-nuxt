dddafdf6cc1d289e17ca6b77bae45f9714884ad3
Jens Küsters <jens@kuesters.net>
2021-07-19 16:04:35 +0200

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -12,10 +12,10 @@
       </div>
     </header>
     <Nuxt />
-    <footer class='text-sm text-gray-600 my-20 px-12 flex flex-grow items-center justify-center'>
-      &copy;{{ currentDate.getFullYear() }} {{ heading }} |
+    <footer class='text-sm text-gray-600 my-20 px-12 text-center'>
+      &copy;{{ currentDate.getFullYear() }} {{ heading }}<br>
       Made with
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class='fill-current w-4 h-4 mx-px'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class='fill-current w-4 h-4 mx-px inline-block'>
         <path fill-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clip-rule="evenodd" />
       </svg>
       in Neuss
